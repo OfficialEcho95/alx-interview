3cf028d00fc2e5751d46741679183c2a5051e4da
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-05 15:46:35 +0100

    pascal's correction

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -8,7 +8,7 @@ def print_triangle(triangle):
     """
     Print the triangle
     """
-    for row in triangle:
+    for (row in triangle):
         print("[{}]".format(",".join([str(x) for x in row])))
 
 
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,23 @@
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the specified number of rows.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: Pascal's triangle as a nested list of integers.
+    """
     if n <= 0:
         return []
 
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
-    return triangle
 
+    return triangle
